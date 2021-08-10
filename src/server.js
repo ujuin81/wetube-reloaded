2bf9fbe06384591717505815d6b806655c3a6eb3
하보영 <@IT->
2021-08-10 10:08:34 +0900

    3.1~3 request, response

--- src/server.js
+++ src/server.js
@@ -3,6 +3,19 @@ import express from "express"
 const PORT = 4000;
 const app = express();
 
+const handleHome = (req, res) => {
+    console.log(`get request / home!!`);
+    return res.send("Welcome Home");
+}
+
+const handleLogin = (req, res) => {
+    console.log(`get request Login`);
+    return res.send("Login page");
+}
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
 const handleListening = ()=> console.log(`server listening on port http://localhost:${PORT}`);
 
 app.listen(4000, handleListening);
