dd353c473c98b6d27edfaac5c2b013aea5726ac6
하보영 <@IT->
2021-08-12 14:12:15 +0900

    #5.4 Variables to Templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,7 @@
-export const trending = (req, res) => res.render("home");
-export const edit = (req, res) => res.render("edit"); 
+export const trending = (req, res) => res.render("home", {pageTitle: "Home"});
+export const edit = (req, res) => res.render("edit", {pageTitle: "Edit"}); 
 
 export const search = (req, res) => res.send("search video page");
-export const see = (req, res) => res.render("watch");
+export const see = (req, res) => res.render("watch", {pageTitle: "Watch"});
 export const remove = (req, res) => res.send("delete video page");
 export const upload = (req, res) => res.send("upload video page");
