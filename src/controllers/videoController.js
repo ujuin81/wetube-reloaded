c6027a02d4e238fe42332d80621dd57750585c65
하보영 <@IT->
2021-08-12 13:54:27 +0900

    partial

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,7 @@
 export const trending = (req, res) => res.render("home");
-export const edit = (req, res) => res.send("edit video"); 
+export const edit = (req, res) => res.render("edit"); 
 
 export const search = (req, res) => res.send("search video page");
-export const see = (req, res) =>{ 
-    console.log(req.params);
-    return res.send(`see video page #${req.params.id}`);
-}
+export const see = (req, res) => res.render("watch");
 export const remove = (req, res) => res.send("delete video page");
 export const upload = (req, res) => res.send("upload video page");
