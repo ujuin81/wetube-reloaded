bcb83b7ea22e7044067eb345958af50b9b47acba
하보영 <@IT->
2021-08-12 14:36:18 +0900

    mixin전 html 구성

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,21 @@
 export const trending = (req, res) => {
-    const videos = [{title: "Video 1"},{title: "Video 2"},{title: "Video 3"}]
+    const videos = [
+        {
+            title: "Video 1",
+            rating: 5,
+            views: 90
+        },
+        {
+            title: "Video 2",
+            rating: 3,
+            views: 3
+        },
+        {
+            title: "Video 3",
+            rating: 4,
+            views: 9000
+        }]
+    ;
     return res.render("home", {pageTitle: "Home", videos});
 };
 export const edit = (req, res) => res.render("edit", {pageTitle: "Edit"}); 
